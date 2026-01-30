058f51c3935d8cbe459d4a2da629b3b8f2ce1acf
Wallflower Willy <modelingstructuralbim@gmail.com>
2026-01-30 16:45:19 +0530

    Update generate_blog_data.cjs

--- scripts/generate_blog_data.cjs
+++ scripts/generate_blog_data.cjs
@@ -1,6 +1,6 @@
 window.blogPosts = [
   {
-    "url": "posts/undefined",
+    "url": "posts/dharmik",
     "image": "assets/images/default-blog.jpg",
     "date": "2026-01-29",
     "description": "Read our latest analysis on undefined",
